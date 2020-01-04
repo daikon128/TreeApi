a94f380feb1e617f4139d658af18575ce6923d64
daikon <daikon@example.com>
2020-01-05 00:50:48 +0900

    Treeの取得処理と確認処理を追加

--- tree-front/src/App.vue
+++ tree-front/src/App.vue
@@ -2,16 +2,31 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <Tree></Tree>
+    <button v-on:click="getTree()"></button>
   </div>
 </template>
 
 <script>
 import Tree from './components/Tree.vue'
+const axios = require('axios');
 
 export default {
   name: 'app',
   components: {
     Tree
+  },
+  methods: {
+    getTree() {
+      let url = '/tree/1'
+      axios.get(url)
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                alert(error);
+              });
+
+    }
   },
 }
 </script>
