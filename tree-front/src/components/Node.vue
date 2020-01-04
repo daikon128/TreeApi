50ea75e7dfe8bc15ef4fe4f20b8c59c7d2f2680c
daikon <daikon@example.com>
2020-01-05 00:20:33 +0900

    画面表示の再帰処理追加

---
+++ tree-front/src/components/Node.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p>{{ node.id }}:{{ node.title }}</p>
+        <ul v-if="node.children.length > 0">
+            <li class="node"
+                v-for="child in node.children"
+                v-bind:key="child.id">
+                <node v-bind:node="child"></node>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+    export default {
+        name: "node",
+        props: {
+            node: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ tree-front/src/components/Tree.vue
@@ -0,0 +1,60 @@
+<template>
+    <node v-bind:node="tree"></node>
+</template>
+
+<script>
+    import Node from './Node.vue'
+    export default {
+        name: "Tree",
+        components: {
+            Node
+        },
+        data() {
+            return {
+                    tree: {
+                        "id": 3,
+                        "parentId": null,
+                        "children": [
+                            {
+                                "id": 10,
+                                "parentId": 3,
+                                "children": [
+                                    {
+                                        "id": 14,
+                                        "parentId": 10,
+                                        "children": [],
+                                        "title": "test",
+                                        "ownProgressValue": 100.0,
+                                        "importance": 1,
+                                        "progress": 100.0
+                                    }
+                                ],
+                                "title": "testUpdateTitle",
+                                "ownProgressValue": 100.0,
+                                "importance": 100,
+                                "progress": 100.0
+                            },
+                            {
+                                "id": 13,
+                                "parentId": 3,
+                                "children": [],
+                                "title": "test",
+                                "ownProgressValue": 0.0,
+                                "importance": 1,
+                                "progress": 0.0
+                            }
+                        ],
+                        "title": "test",
+                        "ownProgressValue": 0.0,
+                        "importance": 1,
+                        "progress": 1.0
+                    }
+                }
+    }
+
+    }
+</script>
+
+<style scoped>
+
+</style>
