1b3304a29a9a42a49d5f937d7fef4e2cd6b5b79a
daikon <daikon@example.com>
2020-01-13 22:06:23 +0900

    SpringSecurity追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,15 +25,19 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation 'org.springframework.boot:spring-boot-starter-security'
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	compile('org.springframework.security:spring-security-web')
+	compile('org.springframework.security:spring-security-config')
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
+	testImplementation 'org.springframework.security:spring-security-test'
 }
 
 tasks.withType<Test> {
